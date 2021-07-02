dd07f3bae050c3c7b478fd600b5cf4dee7093aa6
Suraj Verma <surajverma@rentalcars.com>
2021-07-02 20:04:28 +0100

    add styling and default state for timer

--- src/containers/Quiz/index.js
+++ src/containers/Quiz/index.js
@@ -3,10 +3,25 @@ import { Component } from "react";
 class Quiz extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      timeRemaining: 60,
+    };
   }
 
   render() {
-    return <h1>Quiz Container TODO</h1>;
+    return (
+      <div className="position-absolute top-50 start-50 translate-middle quiz-container w-75">
+        <div className="card">
+          <div className="card-header text-center py-4">
+            <h3>Time Remaining: {this.state.timeRemaining}</h3>
+          </div>
+          <div className="card-body text-center">
+            Render the question and answers here
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
