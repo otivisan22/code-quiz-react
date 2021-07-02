9217f979c339ca2751733b75c05712b03ac4484e
Suraj Verma <surajverma@rentalcars.com>
2021-07-02 19:15:25 +0100

    styled homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <div className="jumbotron text-center">
+        <h1>CODE QUIZ</h1>
+      </div>
+
+      <div className="position-absolute top-50 start-50 translate-middle start-quiz-container">
+        <button className="btn btn-success">START</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
